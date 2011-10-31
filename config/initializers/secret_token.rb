f9fa7014262715a2881b9db7215a20f5cf317d4d
Frédéric König <frederic.konig@gmail.com>
2011-10-31 16:59:37 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tradman::Application.config.secret_token = 'c1bf3410e84d25deb8663d0e6dc2d9e3fd29959c41f56184fcb547edafca6973f4fc633d78323c089dd6546453d336c847dbd8ad25ad472d7ebdea50cfd8a415'
